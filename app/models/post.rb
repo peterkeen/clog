9cdb22f80900e39f4564b4f4b3ca082c109e3ee6
Pete Keen <peter.keen@bugsplat.info>
2012-12-09 13:57:58 -0800

    Delete button, show recipients under post

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,19 +23,21 @@ class Post < ActiveRecord::Base
   def publish!
     self.update_attributes(:published_at => DateTime.now)
 
-    if self.tags.length > 0
-      @recipients = Recipient.tagged_with(self.tags, :any => true).where('unsubscribed_at is null')
-    else
-      @recipients = Recipient.where('unsubscribed_at is null')
-    end
-
-    @recipients.each do |recipient|
+    recipients.each do |recipient|
       POST_EMAIL_QUEUE.push(
         :post_id             => self.id,
         :recipient_email     => recipient.email,
         :recipient_unique_id => recipient.unique_id
       )
     end
+  end
+
+  def recipients
+    if self.tags.length > 0
+      Recipient.tagged_with(self.tags, :any => true).where('unsubscribed_at is null')
+    else
+      Recipient.where('unsubscribed_at is null')
+    end    
   end
 
   def send_preview!(email)
