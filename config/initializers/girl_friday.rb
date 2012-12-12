d1108e08ec6235d96a51550f1d111c61e9edfb4a
Pete Keen <peter.keen@bugsplat.info>
2012-12-12 12:57:53 -0800

    Scale girl_friday to 1 worker

--- config/initializers/girl_friday.rb
+++ config/initializers/girl_friday.rb
@@ -1,3 +1,3 @@
-POST_EMAIL_QUEUE = GirlFriday::WorkQueue.new(:post_email) do |msg|
+POST_EMAIL_QUEUE = GirlFriday::WorkQueue.new(:post_email, :size => 1) do |msg|
   PostMailer.send_post(msg).deliver
 end
